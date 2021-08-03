5d2e095a16b78276e64dc4ba08532fab7165d93f
Maxim Antonov <bol4onok@gmail.com>
2021-08-03 17:10:46 +0300

    fix: name pattern

--- src/main/java/by/antonov/webproject/util/ValidationPattern.java
+++ src/main/java/by/antonov/webproject/util/ValidationPattern.java
@@ -7,7 +7,7 @@ public enum ValidationPattern implements ContextValue {
   VALIDATION_PASSWORD_PATTERN("(?=^.{6,20}$)^([A-zА-яЁё0-9_!@ -]){6,20}$"),
   VALIDATION_ROUTE_PATTERN("(?=^.{6,200}$)^(?=.*[A-zА-яЁё])([^<>])[A-zА-яЁё0-9,. -]+ - [A-zА-яЁё0-9,. -]+$"),
   VALIDATION_TEXT_PATTERN("^([^<>])+$"),
-  VALIDATION_NAME_PATTERN("(?=^.{1,64}$)^([A-zА-яЁё`\"'. -])+$"),
+  VALIDATION_NAME_PATTERN("(?=^.{1,64}$)^([A-zА-яЁё`'. -])+$"),
   VALIDATION_PHONE_PATTERN("(?=^.{7,25}$)^([+]?[0-9]+[( ]*[0-9]+[) ]*[0-9 -]+)$"),
   VALIDATION_PRICE_PATTERN("(?=^.{2,10}$)^([0-9]+)$");
   private final String pattern;
